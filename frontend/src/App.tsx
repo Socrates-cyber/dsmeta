9a7beed2a596f6d3dd63690e105a4215ec4b4a72
Socrates Morais <socrates0506@gmail.com>
2022-09-11 17:06:30 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,13 @@
+import NotificationButton from "./components/notificationButton"
+
 function App() {
   return(
-    <h1>Olá mundo!</h1>
+    <>
+      <h1>Olá mundo!</h1>
+      <NotificationButton />
+      <NotificationButton />
+      
+    </>
   )   
 }
 
